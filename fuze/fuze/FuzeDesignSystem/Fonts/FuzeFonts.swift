9ccfdb3230855c801e4479bc417e604a9b72248f
lazaro10 <lazaro.lima.12@gmail.com>
2022-11-23 23:31:09 -0300

    Add Regular and Bold Roboto fonts

--- fuze/fuze/FuzeDesignSystem/Fonts/FuzeFonts.swift
+++ fuze/fuze/FuzeDesignSystem/Fonts/FuzeFonts.swift
@@ -8,4 +8,20 @@ extension UIFont {
         
         return font
     }
+
+    class func robotoRegular(size: CGFloat) -> UIFont {
+        guard let font = UIFont(name: "Roboto-Regular", size: size) else {
+            return UIFont.systemFont(ofSize: size)
+        }
+
+        return font
+    }
+
+    class func robotoBold(size: CGFloat) -> UIFont {
+        guard let font = UIFont(name: "Roboto-Bold", size: size) else {
+            return UIFont.systemFont(ofSize: size)
+        }
+
+        return font
+    }
 }
--- fuze/fuzeTests/FuzeDesignSystem/Fonts/FuzeFontsTests.swift
+++ fuze/fuzeTests/FuzeDesignSystem/Fonts/FuzeFontsTests.swift
@@ -2,12 +2,28 @@ import XCTest
 @testable import fuze
 
 final class FuzeFontsTests: XCTestCase {
-    func test_robotoMeidum_whenCreated_shouldReturnAValidFont() {
+    func test_robotoMedium_whenCreated_shouldReturnAValidFont() {
         let robotoMedium: UIFont = .robotoMeidum(size: 16)
 
         XCTAssertEqual(robotoMedium.familyName, "Roboto")
         XCTAssertEqual(robotoMedium.fontName, "Roboto-Medium")
         XCTAssertEqual(robotoMedium.pointSize, 16)
+    }
+
+    func test_robotoRegular_whenCreated_shouldReturnAValidFont() {
+        let robotoMedium: UIFont = .robotoRegular(size: 12)
+
+        XCTAssertEqual(robotoMedium.familyName, "Roboto")
+        XCTAssertEqual(robotoMedium.fontName, "Roboto-Regular")
+        XCTAssertEqual(robotoMedium.pointSize, 12)
+    }
+
+    func test_robotoBold_whenCreated_shouldReturnAValidFont() {
+        let robotoMedium: UIFont = .robotoBold(size: 15)
+
+        XCTAssertEqual(robotoMedium.familyName, "Roboto")
+        XCTAssertEqual(robotoMedium.fontName, "Roboto-Bold")
+        XCTAssertEqual(robotoMedium.pointSize, 15)
     }
 }
 
