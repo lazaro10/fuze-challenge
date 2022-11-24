23ece8331c8050dbefe23f8d6ecff56fcb757d24
lazaro10 <lazaro.lima.12@gmail.com>
2022-11-24 02:46:53 -0300

    Adjusts date formatter tests

--- fuze/fuzeTests/FuzeDesignSystem/Formatters/DateFormatterTest.swift
+++ fuze/fuzeTests/FuzeDesignSystem/Formatters/DateFormatterTest.swift
@@ -3,29 +3,29 @@ import XCTest
 
 final class DateFormatterTest: XCTestCase {
     func test_toDateZ_shouldFormatCorrectly() {
-        let sut = "2022-11-23T08:09:40Z".toDateZ()
+        let sut = "2035-10-23T08:09:40Z".toDateZ()
 
         XCTAssertNotNil(sut)
     }
 
     func test_toShortDate_shouldFormatCorrectly() {
-        let date = "2022-11-23T08:09:40Z".toDateZ()
+        let date = "2035-10-23T08:09:40Z".toDateZ()
         let sut = date?.toShortDate()
 
-        XCTAssertNotNil(sut)
+        XCTAssertEqual(sut, "10/23/35, 5:09 AM")
     }
 
     func test_toMediumDate_shouldFormatCorrectly() {
-        let date = "2022-11-23T08:09:40Z".toDateZ()
+        let date = "2035-10-23T08:09:40Z".toDateZ()
         let sut = date?.toMediumDate()
 
-        XCTAssertEqual(sut, "Wed 05:09")
+        XCTAssertEqual(sut, "Tue 05:09")
     }
 
     func test_toLongDate_shouldFormatCorrectly() {
-        let date = "2022-11-23T08:09:40Z".toDateZ()
+        let date = "2035-10-23T08:09:40Z".toDateZ()
         let sut = date?.toLongDate()
 
-        XCTAssertEqual(sut, "23.11 05:09")
+        XCTAssertEqual(sut, "23.10 05:09")
     }
 }
